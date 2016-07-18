d242f15199441b923b5b8fe1b20452940e0b2fc9
cosminctinescu <cosmin.constantinescu@tremend.com>
2016-07-18 19:34:14 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
-from urllib.request import urlopen
-response_body = urlopen("https://10.1.11.227:8080/api/4/apiVer").read()
-print(response_body)
-
-
-
